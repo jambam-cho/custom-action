95095b43cc92ab1d4d5f4fb79b7f0f75285aa375
jambam-cho <hs.cho@bucketplace.net>
2023-06-23 04:48:11 +0900

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import * as core from '@actions/core'
 import * as fs from 'fs'
-import fetch, { Response } from 'node-fetch'
-import * as https from 'https'
 import { promisify } from 'util'
+import { request } from '@octokit/request'
 
 const pipeline = promisify(require('stream').pipeline)
 
@@ -10,83 +9,56 @@ interface Release {
   assets: Array<{ browser_download_url: string }>
 }
 
-async function downloadFile(url: string, outputFilePath: string) {
-  return new Promise<void>(async (resolve, reject) => {
-    const response: Response = await fetch(url)
-
-    if (response.ok) {
-      if (response.body) {
-        const file = fs.createWriteStream(outputFilePath)
-        const downloadStream = response.body.pipe(file)
-
-        downloadStream.on('finish', () => {
-          file.close()
-          resolve()
-        })
-
-        downloadStream.on('error', (err: Error) => {
-          fs.unlinkSync(outputFilePath)
-          reject(err.message)
-        })
-      } else {
-        reject('Response body is null.')
-      }
-    } else {
-      reject(`Unexpected response: ${response.statusText}`)
+async function downloadFile(url: string, outputFilePath: string, authToken: string) {
+  const response = await request("GET " + url, {
+    headers: {
+      Accept: "application/octet-stream",
+      Authorization: `token ${authToken}`
     }
+  });
+
+  const file = fs.createWriteStream(outputFilePath);
+  response.data.pipe(file);
+
+  file.on('finish', () => {
+    file.close();
+  })
+
+  file.on('error', (err: Error) => {
+    fs.unlinkSync(outputFilePath);
+    throw err.message;
   })
 }
 
-async function getAssetUrl(
-  releasesUrl: string,
-  authToken: string | undefined
-): Promise<string> {
-  const headers: HeadersInit = authToken
-    ? { Authorization: `token ${authToken}` }
-    : {}
-  const jsonResponse = await fetch(releasesUrl, {
-    headers
-  })
+async function getAssetUrl(releasesUrl: string, authToken: string): Promise<string> {
+  const response = await request("GET " + releasesUrl, {
+    headers: { Authorization: `token ${authToken}` }
+  });
 
-  if (!jsonResponse.ok) {
-    throw new Error(`Unexpected response: ${jsonResponse.statusText}`)
+  const latestRelease = response.data as Release
+  if (latestRelease && latestRelease.assets && latestRelease.assets[0]) {
+    return latestRelease.assets[0].browser_download_url
   }
-
-  const responseText = (await jsonResponse.text()).replace(/^\uFEFF/, '');
-
-  try {
-    const latestRelease = JSON.parse(responseText) as Release
-    if (latestRelease && latestRelease.assets && latestRelease.assets[0]) {
-      return latestRelease.assets[0].browser_download_url
-    }
-
-    throw new Error('Failed to fetch asset URL')
-  } catch (error) {
-    console.error('An error occurred while parsing the JSON:', error)
-    console.error('Received data:', responseText)
-    throw error;
-  }
+  throw new Error('Failed to fetch asset URL');
 }
-
-
 
 async function run() {
   try {
-    const releasesUrl = core.getInput('releases-url', { required: true })
-    const outputFilePath = core.getInput('output-file-path', { required: true })
-    const authToken = core.getInput('auth-token', { required: false })
+    const releasesUrl = core.getInput('releases-url', { required: true });
+    const outputFilePath = core.getInput('output-file-path', { required: true });
+    const authToken = core.getInput('auth-token', { required: false });
 
-    const assetUrl = await getAssetUrl(releasesUrl, authToken)
-    await downloadFile(assetUrl, outputFilePath)
+    const assetUrl = await getAssetUrl(releasesUrl, authToken);
+    await downloadFile(assetUrl, outputFilePath, authToken);
 
-    core.setOutput('downloaded-file-path', outputFilePath)
+    core.setOutput('downloaded-file-path', outputFilePath);
   } catch (error: unknown) {
     if (error instanceof Error) {
-      core.setFailed(error.message)
+      core.setFailed(error.message);
     } else {
-      core.setFailed('An unexpected error occurred.')
+      core.setFailed('An unexpected error occurred.');
     }
   }
 }
 
-run()
+run();
