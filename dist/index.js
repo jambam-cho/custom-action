d326476395c69f9f12c0dbc6b06b3989b3b5142e
jambam-cho <hs.cho@bucketplace.net>
2023-06-25 14:30:15 +0900

    first commit

--- dist/index.js
+++ dist/index.js
@@ -7003,13 +7003,6 @@ module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("path");
 
 /***/ }),
 
-/***/ 2781:
-/***/ ((module) => {
-
-module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("stream");
-
-/***/ }),
-
 /***/ 4404:
 /***/ ((module) => {
 
@@ -7763,8 +7756,6 @@ var core = __nccwpck_require__(2186);
 // EXTERNAL MODULE: external "fs"
 var external_fs_ = __nccwpck_require__(7147);
 var external_fs_default = /*#__PURE__*/__nccwpck_require__.n(external_fs_);
-// EXTERNAL MODULE: external "util"
-var external_util_ = __nccwpck_require__(3837);
 ;// CONCATENATED MODULE: external "node:http"
 const external_node_http_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("node:http");
 ;// CONCATENATED MODULE: external "node:https"
@@ -9918,8 +9909,6 @@ function fixResponseChunkedTransferBadEnding(request, errorCallback) {
 
 
 
-
-const src_pipeline = (0,external_util_.promisify)((__nccwpck_require__(2781).pipeline));
 async function getLatestRelease(githubRepo, authToken) {
     const url = `https://api.github.com/repos/${githubRepo}/releases/latest`;
     const response = await fetch(url, {
@@ -9943,20 +9932,23 @@ async function getAssetDownloadUrls(githubRepo, authToken, osArchPairs) {
         `${repoName}_${tag}_SHA256SUMS`,
         `${repoName}_${tag}_SHA256SUMS.sig`
     ]);
-    const assets = latestRelease.assets.filter(asset => desiredFileNames.includes(asset.name));
+    const assets = latestRelease.assets
+        .filter(asset => desiredFileNames.includes(asset.name))
+        .map(asset => ({
+        id: asset.id,
+        name: asset.name,
+        browser_download_url: asset.browser_download_url
+    }));
     return assets;
 }
-async function downloadAsset(asset, outputDir
-// authToken: string
-) {
-    const authToken = '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';
-    const response = await fetch(asset.browser_download_url, {
+async function downloadAsset(asset, outputDir, authToken) {
+    const url = `https://api.github.com/repos/jambam-cho/terraform-provider-hashicups/releases/assets/${asset.id}`;
+    const response = await fetch(url, {
         headers: {
             Accept: 'application/octet-stream',
             Authorization: `token ${authToken}`
         }
     });
-    console.log('Download response:', response.status, response.headers);
     if (!response.body) {
         throw new Error('Error: The response body is null.');
     }
@@ -9972,52 +9964,16 @@ async function downloadAsset(asset, outputDir
         });
     }
     else if (response.status === 404) {
-        throw new Error(`File not found: ${asset.browser_download_url}`);
+        throw new Error(`File not found: asset ${asset.id}`);
     }
     else {
         throw new Error(`Unexpected response for downloading file: ${response.status}`);
     }
 }
-// async function downloadAsset(
-//   asset: Asset,
-//   outputDir: string,
-//   authToken: string
-// ) {
-//   console.log('Downloading asset:', asset.browser_download_url)
-//   const response = await request('GET' + asset.browser_download_url, {
-//     headers: {
-//       Accept: 'application/octet-stream',
-//       Authorization: `token ${authToken}`
-//     },
-//     request: {fetch}
-//   })
-//
-//   console.log('Download response:', response.status, response.headers)
-//
-//   const file = fs.createWriteStream(`${outputDir}/${asset.name}`)
-//
-//   if (response.status === 200) {
-//     response.data.pipe(file)
-//
-//     file.on('finish', () => {
-//       file.close()
-//     })
-//
-//     file.on('error', (err: Error) => {
-//       fs.unlinkSync(outputDir)
-//       throw err.message
-//     })
-//   } else {
-//     throw new Error(
-//       `Unexpected response for downloading file: ${response.status}`
-//     )
-//   }
-// }
 async function downloadAssets(assets, outputDir, authToken) {
     for (const asset of assets) {
-        console.log(`Downloading ${asset.browser_download_url}`);
-        // await downloadAsset(asset, outputDir, authToken)
-        await downloadAsset(asset, outputDir);
+        console.log(`Downloading asset: ${asset.id}`);
+        await downloadAsset(asset, outputDir, authToken);
     }
 }
 async function run() {
@@ -10029,7 +9985,7 @@ async function run() {
             .split(',')
             .map(pair => pair.trim());
         const assets = await getAssetDownloadUrls(githubRepo, authToken, osArchPairs);
-        console.log('Asset download URLs:', assets.map(asset => asset.browser_download_url));
+        console.log('Asset IDs:', assets.map(asset => asset.id));
         await downloadAssets(assets, outputDir, authToken);
         console.log('Assets downloaded successfully');
         core.setOutput('downloaded-file-path', outputDir);
